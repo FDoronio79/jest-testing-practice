b3c30490b254f648a6f7c39f57febdb2a7acd539
Filamer Doronio <filamerdoronio@gmail.com>
2024-06-24 20:27:22 -0700

    completed initial set up of project

--- users/src/App.js
+++ users/src/App.js
@@ -1,9 +1,19 @@
+import { useState } from 'react';
 import UserForm from "./userForm";
+import UserList from './userList';
 
 function App() {
+  const [ users, setUsers ] = useState([]);
+
+  const onUserAdd = (user) => {
+    setUsers([...users, user])
+  }
+  
   return (
     <div>
-      <UserForm />
+      <UserForm onUserAdd={onUserAdd}/>
+      <hr />
+      <UserList users={users} />
     </div>
   )
 }
--- users/src/userForm.js
+++ users/src/userForm.js
@@ -1,20 +1,32 @@
-function UserForm() {
+import { useState } from 'react';
 
+function UserForm({ onUserAdd }) {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
-    return <form>
+        onUserAdd({ name, email })
+        setName('');
+        setEmail('');
+    }
+
+    return (
+    <form onSubmit={handleSubmit}>
         <div>
             <label>Name</label>
-            <input />
+            <input value={name} onChange={(e) => setName(e.target.value)}/>
         </div>
         <div>
             <label>Email</label>
-            <input />
+            <input value={email} onChange={(e) => setEmail(e.target.value)} />
         </div>
         <div>
             <button> Add User </button>
         </div>
     </form>
+    )
 }
 
 export default UserForm;

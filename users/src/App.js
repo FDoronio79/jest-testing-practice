cee754dc80c8371c96b9a45ffb41480dd41eb3c3
Filamer Doronio <filamerdoronio@gmail.com>
2024-06-24 20:14:56 -0700

    initial commit

---
+++ users/src/App.js
@@ -0,0 +1,11 @@
+import UserForm from "./userForm";
+
+function App() {
+  return (
+    <div>
+      <UserForm />
+    </div>
+  )
+}
+
+export default App;
---
+++ users/src/userForm.js
@@ -0,0 +1,20 @@
+function UserForm() {
+
+
+
+    return <form>
+        <div>
+            <label>Name</label>
+            <input />
+        </div>
+        <div>
+            <label>Email</label>
+            <input />
+        </div>
+        <div>
+            <button> Add User </button>
+        </div>
+    </form>
+}
+
+export default UserForm;
